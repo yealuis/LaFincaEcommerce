3c0292f3b1f7de31c984c36954b3610e8c3223f1
yealuis <luisjavierperez1999@gmail.com>
2025-05-12 17:03:42 -0400

    agregado de funcionalidad de la seleccion del tamaño de los productos

--- src/components/CustomizeProducts.jsx
+++ src/components/CustomizeProducts.jsx
@@ -1,6 +1,15 @@
+"use client"
+import { useState } from "react"
 import styles from "./customizeProducts.module.css"
 
 const CustomizeProducts = () => {
+  const [size, setSize] = useState(0)
+  const isDisabled = (key) => key === 4
+
+  const handleSizeChange = (key) => {
+      return size === key ? styles.chooseSize : `${styles.chooseSize} ${styles.chooseSizeNotSelected}`
+  }
+
   return (
     <div className={styles.customizeContainer}>
       <h4 className={styles.customizeTitle}>Escoge un color</h4>
@@ -15,9 +24,10 @@ const CustomizeProducts = () => {
       </ul>
       <h4 className={styles.customizeTitle}>Escoge una presentación</h4>
       <ul className={styles.chooseList}>
-        <li className={`${styles.chooseSize}`}>100 ml</li>
-        <li className={`${styles.chooseSize} ${styles.chooseSizeNotSelected}`}>500 ml</li>
-        <li className={`${styles.chooseSize} ${styles.chooseSizeDisable}`}>1 Litro</li>
+        <li className={`${handleSizeChange(1)}`} onClick={() => !isDisabled(1) && setSize(1)}>100 ml</li>
+        <li className={`${handleSizeChange(2)}`} onClick={() => !isDisabled(2) && setSize(2)}>250 ml</li>
+        <li className={`${handleSizeChange(3)}`} onClick={() => !isDisabled(3) && setSize(3)}>500 ml</li>
+        <li className={`${handleSizeChange(4)} ${styles.chooseSizeDisable}`} onClick={() => !isDisabled(4) && setSize(4)}>1 Litro</li>
       </ul>
     </div>
   )
